32606b94108f0a2423eaba7b4019523f6643d844
Kit <withervn1603@gmail.com>
2026-03-12 22:18:46 +0700

    update usersRoutes.js

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
     getUsers,
+    getUserById,
     addUser,
     editUser,
     removeUser
@@ -9,6 +10,7 @@ import {
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUserById);
 router.post("/", addUser);
 router.put("/:id", editUser);
 router.delete("/:id", removeUser);
