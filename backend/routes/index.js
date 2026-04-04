b81e6ca21fe22444ff0a3f1e6e2d40e0d83f284e
Kit <tak1632004@gmail.com>
2026-04-05 04:25:25 +0700

    feat: login

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 import express from "express";
-
 import userRoutes from "./users.route.js";
+import authRoutes from "./auth.routes.js";
 
 const router = express.Router();
 
 export function AppRouter(app) {
-    router.use('/users', userRoutes);
 
-    app.use('/', router);
+    router.use('/users', userRoutes);
+    router.use('/auth', authRoutes);
+    app.use('/api', router);
 };
